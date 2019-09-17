64bd825d9b389ec23721676138636360ea834a3d
Will campbell <w.campbell@student.unimelb.edu.au>
2019-09-17 17:46:19 +1000

    test makefile data

---
+++ makefileTest/simple/add.h
@@ -0,0 +1,21 @@
+//
+// Created by William Campbell on 2019-09-17.
+//
+
+#ifndef EXO_ADD_H
+#define EXO_ADD_H
+
+
+class add {
+private:
+    int a;
+    int c;
+public:
+
+    void setA(int num);
+    void setB(int num);
+    void  adding();
+};
+
+
+#endif //EXO_ADD_H
---
+++ makefileTest/simple/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by William Campbell on 2019-09-17.
+//
+#include "add.h"
+
+
+int main(){
+    add adder;
+    adder.setA(5);
+    adder.setB(10);
+    adder.adding();
+}
