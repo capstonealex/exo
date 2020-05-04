03e8c3a544f351616d078603c53751b1c0bd41d4
unknown <fong.j@unimelb.edu.au>
2020-05-04 15:34:45 +1000

    Added DummyTrajectoryGenerator

---
+++ Refactor/src/apps/DummyTrajectoryGenerator.h
@@ -0,0 +1,54 @@
+/**
+ * @file DummyTrajectoryGenerator.h
+ * @author Justin Fong
+ * @brief A trajectory generator to be used for testing purposes
+ * @version 0.1
+ * @date 2020-05-04
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef DUMMYTRAJECTORYGENERATOR_H_INCLUDED
+#define DUMMYTRAJECTORYGENERATOR_H_INCLUDED
+
+#include "TrajectoryGenerator.h"
+
+enum Trajectory {
+    SIT = 0,
+    STAND = 1,
+};
+
+class DummyTrajectoryGenerator : public TrajectoryGenerator {
+   private:
+    std::vector<double[2]> endPoints;
+    Trajectory currTraj = SIT;
+    double trajTime = 2;
+    int numJoints = 6;
+
+   public:
+    DummyTrajectoryGenerator(int NumOfJoints);
+    /**
+     * @brief Implementation of the initialiseTrajectory method in TrajectoryGenerator
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool initialiseTrajectory();
+
+    /**
+     * @brief 
+     * 
+     */
+    bool initialiseTrajectory(Trajectory traj, double time);
+
+    /**
+     * @brief Implementation of the getSetPoint method in TrajectoryGenerator
+     * @param time The time corresponding to the point. 
+     * 
+     * @return vector<double> 
+     */
+    std::vector<double> getSetPoint(double time);
+};
+
+#endif
